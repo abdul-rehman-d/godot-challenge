f2b457f36cf48300033912b7ad779d301c31d6ae
Abdul Rehman <ar618211@gmail.com>
2025-02-07 19:02:58 +0500

    round 4: animations based on state

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,6 +6,9 @@ extends CharacterBody2D
 @export var short_jump_velocity = 50.0
 
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -23,5 +26,16 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	if velocity.y:
+		animated_sprite_2d.play("jump")
+	elif velocity.x:
+		if velocity.x > 0 and not animated_sprite_2d.flip_h:
+			animated_sprite_2d.flip_h = true
+		elif velocity.x < 0 and animated_sprite_2d.flip_h:
+			animated_sprite_2d.flip_h = false
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
 
 	move_and_slide()
