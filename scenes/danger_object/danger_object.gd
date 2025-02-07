fb6db75ff0557359f12839f5d471aa04b7af5cd8
Abdul Rehman <ar618211@gmail.com>
2025-02-07 20:38:04 +0500

    round 8: danger object 💣

---
+++ scenes/danger_object/danger_object.gd
@@ -0,0 +1,27 @@
+extends StaticBody2D
+
+@export var begin_position: Marker2D
+@export var end_position: Marker2D
+
+@export var speed = 50.0
+
+var target: Vector2 = Vector2.ZERO
+var reverse: bool = false
+
+func _ready() -> void:
+	global_position = begin_position.global_position
+	target = end_position.global_position
+	reverse = false
+
+
+func _process(delta: float) -> void:
+	if global_position.distance_to(target) < 1:
+		# reached
+		if reverse:
+			target = end_position.global_position
+			reverse = false
+		else:
+			target = begin_position.global_position
+			reverse = true
+	
+	global_position = global_position.move_toward(target, speed * delta)
